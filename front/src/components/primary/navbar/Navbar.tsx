d20b26de527ebfe849cd31bd0aa5db11324090c1
_MVMatiasVidela_ <_matias.damian.vi@gmail.com_>
2025-03-04 16:50:05 +0100

    detalles en la navbar

--- front/src/components/primary/navbar/Navbar.tsx
+++ front/src/components/primary/navbar/Navbar.tsx
@@ -11,41 +11,47 @@ const Navbar = () => {
   ];
 
   return (
-    <div>
-      <Box
-        sx={{
-          display: 'flex',
-          paddingY: '12px',
-          border: '1px solid #2B2B2B',
-          paddingX: '24px',
-          borderRadius: '50px',
-          margin: '24px',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Box>
-          <Typography variant="h2">SERVICUCHILLAS</Typography>
-        </Box>
-        <Box sx={{ display: 'flex', gap: '24px' }}>
-          {items.map(item => (
-            <Link key={item.name} href={item.path}>
-              <Typography
-                sx={{
-                  fontWeight: '500',
-                  fontSize: '20px',
-                  ':hover': {
-                    color: '#2B2B2B',
-                  },
-                }}
-              >
-                {item.name}
-              </Typography>
-            </Link>
-          ))}
-        </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        paddingY: '12px',
+        background: '#030712',
+        backdropFilter: 'blur(10px)',
+        zIndex: 10,
+        position: 'fixed',
+        top: 0,
+        left: '50%', // Asegura que el box comience en el centro
+        transform: 'translateX(-50%)', // Ajusta el box para que quede completamente centrado
+        width: '95%', // Esto hace que ocupe el 80% del ancho de la pantalla
+        border: '1px solid #2B2B2B',
+        paddingX: '36px',
+        borderRadius: '50px',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop:'24px'
+      }}
+    >
+      <Box>
+        <Typography variant="h2">SERVICUCHILLAS</Typography>
       </Box>
-    </div>
+      <Box sx={{ display: 'flex', gap: '24px' }}>
+        {items.map(item => (
+          <Link key={item.name} href={item.path}>
+            <Typography
+              sx={{
+                fontWeight: '500',
+                fontSize: '20px',
+                ':hover': {
+                  color: '#2B2B2B',
+                },
+              }}
+            >
+              {item.name}
+            </Typography>
+          </Link>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
