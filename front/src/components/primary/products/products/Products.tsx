077100fe0a9afb038101dcfe9e371418e30b5add
manuochoa16 <nicomanu2316@gmail.com>
2025-03-07 15:59:13 -0300

    products

--- front/src/components/primary/products/products/Products.tsx
+++ front/src/components/primary/products/products/Products.tsx
@@ -1,49 +1,182 @@
-'use client';
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid2';
-import Image from 'next/image';
+"use client";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid2";
+import Image from "next/image";
+import ArrowOutwardIcon from "@mui/icons-material/ArrowOutward";
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#fff',
+  backgroundColor: "#fff",
   ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: 'center',
+  padding: theme.spacing(2),
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+  height: "200px",
   color: theme.palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
+  borderRadius: "16px", // 🔹 Bordes redondeados
+  overflow: "hidden", // 🔹 Evita que la imagen sobresalga del Item
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#111",
   }),
+
+  "&:hover img": {
+    transform: "scale(1.1)", // 🔥 Efecto de agrandar la imagen
+    transition: "transform 0.5s ease-in-out", // ⚡ Suaviza la animación
+  },
 }));
 
 export default function Products() {
   return (
-    <Box sx={{ padding: '100px' }}>
+    <Box sx={{ padding: "100px" }}>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
           <Grid size={8}>
             <Item>
-              <Box>Cuchillas para la industria del plastico</Box>
               <Box>
-                <Image src="" alt="" width={100} height={100}></Image>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359734/cuchillas/gdhufc5kevf2bgtfr3q2.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria del plástico
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
               </Box>
             </Item>
           </Grid>
           <Grid size={4}>
-            <Item>Cuchillas para la industria del papel</Item>
+            <Item>
+              <Box>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359735/cuchillas/ifbyi1lexmfsyi8hwm6f.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria del papel
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
+              </Box>
+            </Item>
           </Grid>
           <Grid size={4}>
-            <Item>Cuchillas para la industria de la madera</Item>
+            <Item>
+              <Box>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359739/cuchillas/mjw1s4ibx4op0uqwccvv.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria de la madera
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
+              </Box>
+            </Item>
           </Grid>
           <Grid size={8}>
-            <Item>size=8</Item>
+            <Item>
+              <Box>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359736/cuchillas/xuilpvnrrbpz8vd0tocf.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria del caucho
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
+              </Box>
+            </Item>
           </Grid>
           <Grid size={8}>
-            <Item>size=8</Item>
+            <Item>
+              <Box>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359738/cuchillas/o9h5lauapb1mwyiylsaw.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria metalmecánica
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
+              </Box>
+            </Item>
           </Grid>
           <Grid size={4}>
-            <Item>size=8</Item>
+            <Item>
+              <Box>
+                <Image
+                  src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359738/cuchillas/ctfhuj48yehxjgzljayy.png"
+                  alt=""
+                  width={100}
+                  height={100}
+                />
+              </Box>
+              <Box
+                sx={{
+                  textAlign: "left",
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                }}
+              >
+                Cuchillas para la industria de alimentos
+                <ArrowOutwardIcon sx={{ fontSize: 24, marginLeft: "8px" }} />
+              </Box>
+            </Item>
           </Grid>
         </Grid>
       </Box>
--- front/src/components/primary/products/recomendations/Recomendations.tsx
+++ front/src/components/primary/products/recomendations/Recomendations.tsx
@@ -1,11 +1,81 @@
-import React from 'react'
+"use client";
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import Image from "next/image";
 
 const Recomendations = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        padding: "50px",
+      }}
+    >
+      {/* Sección de Imágenes */}
+      <Box sx={{ flex: 1, display: "flex", justifyContent: "center", gap: 2 }}>
+        <Image
+          src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359734/cuchillas/gdhufc5kevf2bgtfr3q2.png"
+          alt="Box 1"
+          width={200}
+          height={200}
+          style={{ transform: "rotate(-10deg)" }}
+        />
+        <Image
+          src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359739/cuchillas/mjw1s4ibx4op0uqwccvv.png"
+          alt="Box 2"
+          width={200}
+          height={200}
+          style={{ transform: "rotate(5deg)" }}
+        />
+        <Image
+          src="https://res.cloudinary.com/dzhh3k5kj/image/upload/v1741359735/cuchillas/ifbyi1lexmfsyi8hwm6f.png"
+          alt="Box 3"
+          width={200}
+          height={200}
+          style={{ transform: "rotate(10deg)" }}
+        />
+      </Box>
 
-export default Recomendations
+      {/* Sección de Texto */}
+      <Box sx={{ flex: 1, color: "white", padding: "20px" }}>
+        <Typography variant="h4" fontWeight="bold">
+          ¿Por qué elegir ServiCuchillas?
+        </Typography>
+        <Typography variant="body1" sx={{ marginTop: "10px", opacity: 0.8 }}>
+          Convierte tus ideas en resultados precisos con las cuchillas de
+          ServiCuchillas. Ya sea que necesites herramientas de corte para la
+          industria metalúrgica, papelera, maderera o plástica, nuestras
+          cuchillas están diseñadas para ofrecer máxima precisión y durabilidad.
+        </Typography>
+
+        {/* Lista de Beneficios */}
+        <List sx={{ marginTop: "20px" }}>
+          {[
+            "Cuchillas robustas y resistentes para un rendimiento de larga duración.",
+            "Soluciones innovadoras adaptadas a las necesidades de cada industria.",
+            "Soporte especializado para garantizar el éxito de tus proyectos.",
+          ].map((text, index) => (
+            <ListItem key={index} sx={{ padding: 0 }}>
+              <ListItemIcon>
+                <CheckCircleOutlineIcon sx={{ color: "#F45C43" }} />
+              </ListItemIcon>
+              <ListItemText primary={text} sx={{ color: "white" }} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Box>
+  );
+};
+
+export default Recomendations;
