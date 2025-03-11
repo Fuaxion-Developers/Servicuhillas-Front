d5ff62e5e71ff75360b4963db19d1edc52e26f21
Carlos Tunjano <caratungu@gmail.com>
2025-03-11 17:44:30 -0500

    ADD/ Map component

--- front/src/components/primary/contact/IndexContact.tsx
+++ front/src/components/primary/contact/IndexContact.tsx
@@ -1,18 +1,22 @@
 import Title from "./title/Title";
 import Form from "./form/Form";
 import Info from "./info/Info";
+import Map from "./map/Map";
 import { Box } from "@mui/material";
 
 const IndexContact = () => {
   return (
-    <Box>
+    <Box sx={{
+      paddingX: '100px'
+    }}>
       <Title></Title>
       <Box sx={{
-        display: 'flex'
+        display: 'flex',
       }}>
         <Form></Form>
         <Info></Info>
       </Box>
+      <Map></Map>
     </Box>
   );
 };
