75e37e1c87db14c726160c091dd16be4b0bc267a
Carlos Tunjano <caratungu@gmail.com>
2025-03-12 18:04:09 -0500

    ADD/ Components

--- front/src/app/about/page.tsx
+++ front/src/app/about/page.tsx
@@ -1,5 +1,12 @@
+import IndexAbout from '@/components/primary/about/IndexAbout'
+import { Box } from '@mui/material';
+
 const page = () => {
-  return <div></div>;
+  return (
+  <Box>
+    <IndexAbout></IndexAbout>
+  </Box>
+  );
 };
 
 export default page;
