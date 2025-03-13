d917c35a772c8219f00bfc432baa47c75a6d3433
Carlos Tunjano <caratungu@gmail.com>
2025-03-12 23:33:15 -0500

    ADD/ types to send emails

---
+++ front/src/types/email.ts
@@ -0,0 +1,15 @@
+export type EmailT = {
+    subject: string;
+    from: string;
+    email: string;
+    phone: string;
+    content: string;
+}
+
+export type FormT = {
+    name?: string;
+    lastname?: string;
+    email?: string;
+    phone?: string;
+    content?: string;
+}
